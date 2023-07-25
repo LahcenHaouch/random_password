e87c26c0254e171f1df991f7722759bd972795f9
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-07-25 23:45:29 +0200

    Update css layout to match figma

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const PASSWORD = 'password'
-const [NUM_PASSWORDS, LIMIT, MIN, MAX] = [4, 12, 33, 126]
+const PASSWORD = "password";
+const [NUM_PASSWORDS, LIMIT, MIN, MAX] = [2, 12, 33, 126];
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -8,30 +8,36 @@ function getRandomIntInclusive(min, max) {
 }
 
 function escapeForHtml(str) {
-  return str.replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
+  return str
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
 }
 
 function generateRandomPassword(min, max, limit) {
-  let password = ''
+  let password = "";
 
   for (let i = 0; i < limit; i++) {
-    password += String.fromCharCode(getRandomIntInclusive(min, max))
+    password += String.fromCharCode(getRandomIntInclusive(min, max));
   }
 
-  return password
+  return password;
 }
 
-const button = document.querySelector('button')
-const passwordsContainer = document.querySelector('.passwords-container')
+const button = document.querySelector("button");
+const passwordsContainer = document.querySelector(".passwords-container");
 
-button.addEventListener('click', () => {
-  passwordsContainer.innerHTML = ''
+button.addEventListener("click", () => {
+  passwordsContainer.innerHTML = "";
 
   for (let i = 0; i < NUM_PASSWORDS; i++) {
-    passwordsContainer.insertAdjacentHTML('beforeend', `<div><p class="grid-item">${escapeForHtml(generateRandomPassword(MIN, MAX, LIMIT))}</p></div>`)
+    passwordsContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="grid-item">${escapeForHtml(
+        generateRandomPassword(MIN, MAX, LIMIT)
+      )}</div>`
+    );
   }
-})
+});
